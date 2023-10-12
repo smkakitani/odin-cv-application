0404e19f4099da3046d60077cab996655eac13c9
kakisaura <82690252+kakisaura@users.noreply.github.com>
2023-10-12 16:14:36 -0300

    Create sample data

---
+++ src/components/Data.jsx
@@ -0,0 +1,33 @@
+export const userData = {
+  name: 'Meowster Purrfect',
+  email: 'myemail@mail.com',
+  phone: '(11)1234-1234',
+  url: 'https://exemple.com',
+  education: {
+    name: 'Meowster University',
+    degree: 'Bachelor of Science in Computer Science',
+    year: 'Sep. 2014 - May 2018',
+    address: 'City, State'
+  },
+  experience: {
+    company: 'Cat Café',
+    title: 'Cat Commander',
+    duration: 'Jan. 1999 - Jan. 2019',
+    address: 'City, State',
+    description: 'Developed a service to automatically perform a set of unit tests daily on a product in development in order to decrease time needed for team members to identify and fix bugs/issues.'
+  },
+  project: {
+    name: 'Weather App',
+    tech: 'HTML/CSS, JavaScript, Ruby',
+    date: 'October 2023',
+    description: 'Developed an automatic bot using Python and Google Cloud Console to register myself for a timeslot at my school gym.'
+  },
+  skill: {
+    languages: 'Python, Java, HTML/CSS, JavaScript, Ruby',
+    technologies: 'VS Code, Linux, Git/GitHub'
+  },
+  other: {
+    languages: 'English, Brazilian Portuguese, Japanese',
+    tools: 'Photoshop, Figma, AutoCad'
+  }
+};
