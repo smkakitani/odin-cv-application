811a5ed8917dbdc05e8a35cffb4b2ed153a2df6c
kakisaura <82690252+kakisaura@users.noreply.github.com>
2023-10-13 19:01:08 -0300

    Fix function

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-function ExperienceForm() {
+function ExperienceForm({ handleChange }) {
   return (
     <div>
       <fieldset>
@@ -6,23 +6,33 @@ function ExperienceForm() {
           <legend>Experience details</legend>
           <div>
             <label for="company-name">Company name: </label>
-            <input type="text" name="company-name" id="company-name" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="Cat café"/>
+            <input 
+            onChange={handleChange}
+            type="text" name="company" id="company-name" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="Cat café"/>
           </div>
           <div>
             <label for="job-title">Job title: </label>
-            <input type="text" name="job-title" id="job-title" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="Cat commander"/>
+            <input 
+            onChange={handleChange}
+            type="text" name="title" id="job-title" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="Cat commander"/>
           </div>
           <div>
             <label for="job-duration">Job duration: </label>
-            <input type="text" name="job-duration" id="job-duration" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="Jan. 1999 - Jan 2019"/>
+            <input 
+            onChange={handleChange}
+            type="text" name="jduration" id="job-duration" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="Jan. 1999 - Jan 2019"/>
           </div>
           <div>
             <label for="job-address">Job address: </label>
-            <input type="text" name="job-address" id="job-address" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="City, State"/>
+            <input 
+            onChange={handleChange}
+            type="text" name="address" id="job-address" maxLength="32" pattern="[A-Za-z]{1,32}" placeholder="City, State"/>
           </div>
           <div>
             <label for="job-description">Job description: </label>
-            <textarea name="job-description" id="job-description" cols="30" rows="10"></textarea>
+            <textarea 
+            onChange={handleChange}
+            name="description" id="job-description" cols="30" rows="10"></textarea>
           </div>
         </form>
       </fieldset>
